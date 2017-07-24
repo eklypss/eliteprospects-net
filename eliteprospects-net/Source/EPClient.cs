7d4fc00ac76d7f2bc5877f24c0a7a3a96ebbaf86
eklypss <eklypssdev@gmail.com>
2017-07-24 15:36:04 +0300

    Implemented IDisposable

--- eliteprospects-net/Source/EPClient.cs
+++ eliteprospects-net/Source/EPClient.cs
@@ -15,38 +15,56 @@ namespace eliteprospects_net
 
         public RetiredNumber GetRetiredNumber(Parameters parameters)
         {
-            var result = requester.GetResult(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.RetiredNumbers, parameters)));
-            return JsonConvert.DeserializeObject<RetiredNumber>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = requester.GetResult(request);
+                return JsonConvert.DeserializeObject<RetiredNumber>(result);
+            }
         }
 
         public async Task<RetiredNumber> GetRetiredNumberAsync(Parameters parameters)
         {
-            var result = await requester.GetResultAsync(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.RetiredNumbers, parameters)));
-            return JsonConvert.DeserializeObject<RetiredNumber>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = await requester.GetResultAsync(request);
+                return JsonConvert.DeserializeObject<RetiredNumber>(result);
+            }
         }
 
         public Transfer GetTransfer(Parameters parameters)
         {
-            var result = requester.GetResult(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)));
-            return JsonConvert.DeserializeObject<Transfer>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = requester.GetResult(request);
+                return JsonConvert.DeserializeObject<Transfer>(result);
+            }
         }
 
         public async Task<Transfer> GetTransferAsync(Parameters parameters)
         {
-            var result = await requester.GetResultAsync(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)));
-            return JsonConvert.DeserializeObject<Transfer>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = await requester.GetResultAsync(request);
+                return JsonConvert.DeserializeObject<Transfer>(result);
+            }
         }
 
         public PlayerStatisticalData GetStatisticalData(Parameters parameters)
         {
-            var result = requester.GetResult(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)));
-            return JsonConvert.DeserializeObject<PlayerStatisticalData>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = requester.GetResult(request);
+                return JsonConvert.DeserializeObject<PlayerStatisticalData>(result);
+            }
         }
 
         public async Task<PlayerStatisticalData> GetStatisticalDataAsync(Parameters parameters)
         {
-            var result = await requester.GetResultAsync(new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)));
-            return JsonConvert.DeserializeObject<PlayerStatisticalData>(result);
+            using (var request = new HttpRequestMessage(HttpMethod.Get, requester.BuildString(RequestType.Transfers, parameters)))
+            {
+                var result = await requester.GetResultAsync(request);
+                return JsonConvert.DeserializeObject<PlayerStatisticalData>(result);
+            }
         }
     }
 }
